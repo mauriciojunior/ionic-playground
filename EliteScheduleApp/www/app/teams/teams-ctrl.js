baf66df3864e5bb667c9c18fe13c7cff47d49fd7
Maurício Júnior <mjuniior92@gmail.com>
2016-05-08 19:15:38 -0300

    Update teams-ctrl.js

--- EliteScheduleApp/www/app/teams/teams-ctrl.js
+++ EliteScheduleApp/www/app/teams/teams-ctrl.js
@@ -1,26 +1,14 @@
-(function(window, document, undefined) {
-	'use strict';
+(function() {
+	'use strict'
 
-	angular.module('eliteApp').controller('TeamsCtrl', ['eliteApi', TeamsCtrl]);
-
+	angular.module('eliteApp')
+	  .controller('TeamsCtrl', TeamsCtrl)
+	  
+  TeamsCtrl.$injector = ['eliteApi']
 	function TeamsCtrl(eliteApi) {
-		var vm = this;
+		var vm = this
 
-		var data = eliteApi.getLeagueData();
-		vm.teams = data.teams;
+		var data = eliteApi.getLeagueData()
+		vm.teams = data.teams
 	}
-})(window, document);
-
-// (function(window, document, undefined) {
-// 	'use strict';
-
-// 	angular.module('eliteApp').controller('TeamsCtrl', ['eliteApi', TeamsCtrl]);
-
-// 	function TeamsCtrl(eliteApi, $ionicScroll) {
-// 		var vm = this;
-
-// 		eliteApi.getLeagueData(function (data) {
-// 			vm.teams = data.teams;
-// 		});
-// 	}
-// })(window, document);
+})()
